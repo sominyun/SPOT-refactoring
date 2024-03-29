13d39af8fd92a25ce176f35bc7660243fb45619d
Somin Yun <somin455@naver.com>
2024-03-29 19:20:58 +0900

    fix: JpaConfig 추가

--- Api-Module/src/main/java/com/spot/refactoring/ApiApplication.java
+++ Api-Module/src/main/java/com/spot/refactoring/ApiApplication.java
@@ -2,12 +2,8 @@ package com.spot.refactoring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "com.spot.refactoring")
-@EntityScan(basePackages = "com.spot.refactoring")
-@EnableJpaRepositories(basePackages = {"com.spot.refactoring"})
 public class ApiApplication {
 
     public static void main(String[] args) {
